d0bf9417abb046d445c4b2ab9b3c8f3047a9b497
kokorin.e <zhe-k@yandex.ru>
2022-09-06 16:26:38 +0300

    DZ_1

--- dz_1.5.py
+++ dz_1.5.py
@@ -2,4 +2,5 @@ x1 = float(input('Введите X1: '))
 y1 = float(input('Введите Y1: '))
 x2 = float(input('Введите X2: '))
 y2 = float(input('Введите Y2: '))
-print((((x1-x2)**2+(y1-y2)**2)**0.5))
+s = (((x1-x2)**2+(y1-y2)**2)**0.5)*1000
+print(round(s)/1000)
